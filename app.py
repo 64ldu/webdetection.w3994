8f4dd52d0d2bcccb253832faceee2ec90b0c6147
nekoai0 <kylons0000@gmail.com>
2026-01-28 21:29:51 +0100

    Fix Werkzeug production error: add allow_unsafe_werkzeug=True

--- app.py
+++ app.py
@@ -146,4 +146,4 @@ def handle_frame(data):
 if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 8080))
-    socketio.run(app, debug=False, host='0.0.0.0', port=port)
+    socketio.run(app, debug=False, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
